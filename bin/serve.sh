7b1a5e5b5351c7cf2de20aaeb58900d1ab7b0d46
Muhammad Nizar <mnizarzr@gmail.com>
2023-04-17 11:50:35 +0700

    vite assets

--- bin/serve.sh
+++ bin/serve.sh
@@ -4,8 +4,6 @@ set -o errexit
 
 
 bundle install
-bin/vite clobber --mode=production
-RAILS_ENV=production bin/rake assets:precompile
 bundle exec rake assets:clean
 bundle exec rake db:migrate
 bundle exec puma -C config/puma.rb
